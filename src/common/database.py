20dfc28f9095dc45f313b8b3699141b396a5c275
skykitten2y <57879006+skykitten2y@users.noreply.github.com>
2019-11-17 22:32:32 -0500

    Update database.py

--- src/common/database.py
+++ src/common/database.py
@@ -3,8 +3,7 @@ import os
 import pymongo
 
 class Database(object):  #get general Database class as well as object you defined below
-    uri = mongo ds063180.mlab.com:63180/heroku_90spfz55 -u admin -p wethebest479
-    # os.environ.get("MONGOLAB_URI")
+    uri = os.environ.get("MONGOLAB_URI")
     DATABASE = None
     # with out initialize method, since all database need to access the same uri and database
 
