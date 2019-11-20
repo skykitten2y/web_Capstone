f47abec9f96ea8fd69ba49a4c72079b6ce62610f
skykitten2y <57879006+skykitten2y@users.noreply.github.com>
2019-11-20 12:23:26 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -135,7 +135,7 @@ def confirm_survey():
 @app.route('/portfolio', methods=['POST'])
 def portfolio_options():
     if (session.get('answer_q16', None) == "Get the risk and return profile for given portfolio"):
-        return render_template("test.html") #replace given_portfolio_ask.html
+        return render_template("given_portfolio_ask.html")
     elif (session.get('answer_q16', None) == "Get the optimal portfolio without return"):
         return render_template("portfolio_without_return.html")
     elif (session.get('answer_q16', None) == "Get the optimal portfolio with return"):
@@ -147,7 +147,7 @@ def portfolio_options():
 def existing_portfolio_options():
     e_answer_q16 = request.form.get('e_q16')
     if (e_answer_q16 == "Get the risk and return profile for given portfolio"):
-        return render_template("given_portfolio_ask.html")
+        return render_template("test.html") #given_portfolio_ask.thml
     elif (e_answer_q16 == "Get the optimal portfolio without return"):
         return render_template("portfolio_without_return.html")
     elif (e_answer_q16 == "Get the optimal portfolio with return"):
