584ccc47f28e4d8681a774fba29fa52c55897921
skykitten2y <57879006+skykitten2y@users.noreply.github.com>
2019-12-04 04:38:32 -0500

    d

--- src/app.py
+++ src/app.py
@@ -228,7 +228,9 @@ def portfolio_options():
 
 @app.route('/existing_portfolio',methods = ['POST'])
 def existing_portfolio_options():
+
     e_answer_q16 = request.form.get('e_q16')
+    session['e_answer_q16'] = e_answer_q16
 
     if (e_answer_q16 == "Get the risk and return profile for given portfolio"):
         input_portfolio = Database.find_one("surveys", {"email": session.get('email', None)})['given_portfolio']
@@ -247,7 +249,8 @@ def existing_portfolio_options():
 
 
         return render_template("given_portfolio_ask.html", portfolio=portfolio) #given_portfolio_ask.thml
-    elif (e_answer_q16 == "Get the optimal portfolio without return"):
+
+    elif (e_answer_q16 == "Enhance the existing portfolio"):
         input_portfolio = Database.find_one("surveys", {"email": session.get('email', None)})['given_portfolio']
         stocks = []
         weights = []
@@ -262,8 +265,8 @@ def existing_portfolio_options():
             portfolio.append(stocks[i])
             portfolio.append(weights[i])
 
-
         return render_template("function2_input_portfolio.html" ,portfolio = portfolio)
+
     elif (e_answer_q16 == "Get the optimal portfolio with return"):
         return render_template("portfolio_with_return_ask.html")
     else:
@@ -551,8 +554,7 @@ def store_investor_expected_return():
     portfolio_var = np.var(portfolio_return_list)
     port_sharpe_ratio = (portfolio_return - riskfree) / np.sqrt(portfolio_var)
 
-    sr = 0.8
-        # round(port_sharpe_ratio, 2)  # input sharpe ratio from portfolio
+    sr = round(port_sharpe_ratio, 2)  # input sharpe ratio from portfolio
     # sp500_sr_1y = 5.4
     # sp500_sr_3y = 1.07
     # sp500_sr_5y = 0.83
@@ -585,37 +587,65 @@ def input_weight_portfolio():
 # add
 @app.route('/portfolio/input_portfolio_confirm',methods=['POST'])
 def input_confirm_portfolio():
-    input_weight = request.form.get('input_weight')
-    input_weight = input_weight.split(",")
-    input_portfolio = session.get('input_portfolio',None)
-    input_backtest_period = request.form.get('input_backtest_period')
-    if (input_backtest_period == ""):
-        input_backtest_period = 10
+    if(session.get('e_answer_q16_',None) == "Enhance the existing portfolio"):
+        input_weight = request.form.get('input_weight')
+        input_weight = input_weight.split(",")
+        input_portfolio = session.get('input_portfolio', None)
 
-    # session['input_backtest_period'] = input_backtest_period
+        input_backtest_period = request.form.get('input_backtest_period_2')
+        if (input_backtest_period == ""):
+            input_backtest_period = 10
+        session['input_backtest_period'] = input_backtest_period
 
-    updated_survey = Database.find_one("surveys", {"email": session.get('email', None)})
-    updated_survey['back_time'] = int(input_backtest_period)
-    Database.replace_data('surveys', {"email": session.get('email', None)}, updated_survey)
+        temp = 0
+        given_portfolio = {}
+        for i in input_portfolio:
+            given_portfolio[i] = float(input_weight[temp])
+            temp = temp + 1
 
+        session['given_portfolio'] = given_portfolio
 
+        updated_survey = Database.find_one("surveys", {"email": session.get('email', None)})
+        updated_survey["given_portfolio"] = given_portfolio
+        Database.replace_data('surveys', {"email": session.get('email', None)}, updated_survey)
 
-
-    temp= 0
-    given_portfolio = {}
-    for i in input_portfolio:
-        given_portfolio[i]= float(input_weight[temp])
-        temp = temp + 1
-
-    session['given_portfolio'] = given_portfolio
-
-
-    updated_survey = Database.find_one("surveys", {"email":session.get('email',None)})
-    updated_survey["given_portfolio"]= given_portfolio
-    Database.replace_data('surveys',{"email":session.get('email',None)}, updated_survey)
+        return render_template("function2_given_portfolio_results.html", input_portfolio=input_portfolio,
+                               input_weight=input_weight, given_portfolio=given_portfolio)
 
 
-    return render_template("given_portfolio_results.html", input_portfolio = session.get('input_portfolio',None),input_weight=input_weight,given_portfolio=given_portfolio)
+    else:
+
+        input_weight = request.form.get('input_weight')
+        input_weight = input_weight.split(",")
+        input_portfolio = session.get('input_portfolio',None)
+        input_backtest_period = request.form.get('input_backtest_period')
+        if (input_backtest_period == ""):
+            input_backtest_period = 10
+
+        # session['input_backtest_period'] = input_backtest_period
+
+        updated_survey = Database.find_one("surveys", {"email": session.get('email', None)})
+        updated_survey['back_time'] = int(input_backtest_period)
+        Database.replace_data('surveys', {"email": session.get('email', None)}, updated_survey)
+
+
+
+
+        temp= 0
+        given_portfolio = {}
+        for i in input_portfolio:
+            given_portfolio[i]= float(input_weight[temp])
+            temp = temp + 1
+
+        session['given_portfolio'] = given_portfolio
+
+
+        updated_survey = Database.find_one("surveys", {"email":session.get('email',None)})
+        updated_survey["given_portfolio"]= given_portfolio
+        Database.replace_data('surveys',{"email":session.get('email',None)}, updated_survey)
+
+
+        return render_template("given_portfolio_results.html", input_portfolio = session.get('input_portfolio',None),input_weight=input_weight,given_portfolio=given_portfolio)
 
 
 
